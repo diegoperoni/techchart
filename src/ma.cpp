fd7fcdf9b2470f880afe4f7117abdc582ea8dcb7
diegoperoni <diegoperoni1971@gmail.com>
2020-12-27 14:38:29 +0100

    Update ma.cpp

--- src/ma.cpp
+++ src/ma.cpp
@@ -86,3 +86,22 @@ Rcpp::NumericVector ma(NumericVector x, int k, String weighting) {
   
   return out;
 }
+
+// [[Rcpp::export]]
+Rcpp::NumericVector adaptiveEMA(NumericVector prices, NumericVector alpha) {
+    
+    Rcpp::NumericVector out = clone(prices);
+    int n = out.size();
+    
+    /* Set leading NAs in output */
+    for(i = 0; i < n; i++) {
+      out[i] = 0;
+    }
+    
+    /* Loop over non-NA input values */
+    for(i = 1; i < n; i++) {
+        out[i] = prices[i] * alpha[i] + out[i-1] * (1 - alpha[i])
+    }
+    
+    return out;
+}
